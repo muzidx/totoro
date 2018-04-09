a494295bf2b98a387cb9d8c169d5857bb9240a09
muzidx <wwwxuer@126.com>
2018-04-09 12:40:47 +0800

    feat(v3): style tasks for build common css

--- package/v3/gulp/tasks/style.js
+++ package/v3/gulp/tasks/style.js
@@ -2,19 +2,19 @@
 * @Author: muzidx
 * @Date:   2018-04-09 11:16:21
 * @Last Modified by:   muzidx
-* @Last Modified time: 2018-04-09 11:19:46
+* @Last Modified time: 2018-04-09 12:39:01
 */
 
 const path = require('path')
+const gulp = require('gulp')
 const sass = require('gulp-sass')
 const base64 = require('gulp-base64')
+const rename = require('gulp-rename')
 
-const src = './static_resource/common/css/base.css'
-const dest = './static'
-
-gulp.task('launch', function () {
-  gulp.src(src)
+gulp.task('common:css', function () {
+  gulp.src('./static_resource/common/css/base.scss')
       .pipe(sass({ outputStyle: 'compressed' }))
       .pipe(base64({ debug: true }))
-      .pipe(gulp.dest(dest))
+      .pipe(rename('common.css'))
+      .pipe(gulp.dest('./static'))
 })
