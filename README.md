bc508d2671b054c5bf7534fe608e2bf18e809589
muzidx <wwwxuer@126.com>
2018-04-09 18:11:11 +0800

    chore: update readm & fix bug

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 totoro: 龙猫（日語：となりのトトロ）是吉卜力工作室与德间书店於1988年推出的一部动画电影，由宮崎駿所執導 (via:https://movie.douban.com/subject/1291560/)
 
 ![alt totoro](https://image.ibb.co/mGasXH/totoro.png)
+
+### Usage
+`package` 中提供了我从事前端以来使用过的编译工作流，目前有 v1, v2, v3 版本
+详细区别可参考 [待完成]() 
+
+### cli
+提供了命令行工具，可以选择任一版本的工作流来创建项目
+`totoro v1|v2|v3 -n <project_name>`
--- cli/index.js
+++ cli/index.js
@@ -2,17 +2,16 @@
 * @Author: muzidx
 * @Date:   2018-04-09 15:13:29
 * @Last Modified by:   muzidx
-* @Last Modified time: 2018-04-09 17:46:52
+* @Last Modified time: 2018-04-09 18:10:27
 */
 
 const path = require('path')
-const fs = require('fs')
-const copydir = require('copy-dir')
+const fs = require('fs-extra')
 const chalk = require('chalk')
 const pkgJson = require('../package.json')
 const program = require('commander')
+const shell = require('shelljs')
 
-const cwd = process.cwd()
 const script = process.argv[2]
 const args = process.argv.slice(3)
 
@@ -25,14 +24,19 @@ program.parse(process.argv)
 
 // create target
 let selectVersion = script
-let projectName = args[1] || 'project-example'
-let installPath = path.resolve(cwd, `./package/${selectVersion}`)
-console.log(installPath)
+let projectName = args[1]
+const releaseSource = 'https://github.com/muzidx/totoro/archive/v1.0.0.tar.gz'
+const root = path.resolve(projectName)
 
-if (selectVersion) {
-  fs.mkdirSync(path.resolve(projectName))
-  copydir.sync(installPath, path.resolve(projectName))
+if (selectVersion && projectName) {
+  shell.exec(`wget ${releaseSource} --no-check-certificate -P ${root}`)
+  shell.exec(`tar xzvf ${root}/v1.0.0.tar.gz -C ${root}`)
+  fs.copySync(`${root}/totoro-1.0.0/package/${selectVersion}`, root)
+  shell.exec(`rm -rf ${root}/v1.0.0.tar.gz`)
+  shell.exec(`rm -rf ${root}/totoro-1.0.0`)
+
+  console.log(chalk.bold.green('Congratulations!!! create project success 🐱'))
+} else {
+  console.log(chalk.bold.red('please check the cli param, or use `totoro -h` to view the command!'))
+  process.exit(1)
 }
-
-console.log(chalk.bold.red('please check the cli param, or use `totoro -h` to view the command!'))
-process.exit(1)
