7b212940c21a69cdd0e0558645f3a8bbdcff8807
muzidx <wwwxuer@126.com>
2018-04-10 16:08:29 +0800

    feat(v2): webpack.config.js single entry

--- package/v2/webpack.config.js
+++ package/v2/webpack.config.js
@@ -2,7 +2,7 @@
 * @Author: muzidx
 * @Date:   2018-04-08 16:03:18
 * @Last Modified by:   muzidx
-* @Last Modified time: 2018-04-08 16:31:55
+* @Last Modified time: 2018-04-10 16:07:57
 */
 
 const path = require('path')
@@ -13,10 +13,24 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/assets/',
-    library: 'main.js',
     libraryTarget: 'umd'
   },
   module: {
-
-  }
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: node_modules,
+        loader: 'babcl-loader',
+        options: {
+          presets: ["es2015"]
+        }
+      }
+    ]
+  },
+  plugins: [],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    modules: ['node_modules', path.resolve(__dirname, 'src')]
+  },
+  target: 'web'
 }
