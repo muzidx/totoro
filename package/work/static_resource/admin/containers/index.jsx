fc628339690bdd347109e42d3b98ba76b7687168
muzidx <wwwxuer@126.com>
2018-06-25 17:58:57 +0800

    feat: supplement work redux structure

---
+++ package/work/static_resource/admin/containers/index.jsx
@@ -0,0 +1,15 @@
+import './style.scss'
+
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+
+class Admin extends Component {
+  render () {
+    return (
+      <div className="admin-container">hello totoro</div>
+    )
+  }
+}
+
+export default Admin
---
+++ package/work/static_resource/admin/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Admin from './containers/index'
+
+
+ReactDOM.render(
+  <Admin />,
+  document.getElementById('app')
+)
