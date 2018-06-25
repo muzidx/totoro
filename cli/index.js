fa2eff3a5ac768feea185c0c3b49c442e30b2a8f
muzidx <wwwxuer@126.com>
2018-06-25 18:34:50 +0800

    feat: support local create repo

--- cli/index.js
+++ cli/index.js
@@ -1,42 +1,23 @@
-/*
-* @Author: muzidx
-* @Date:   2018-04-09 15:13:29
-* @Last Modified by:   muzidx
-* @Last Modified time: 2018-04-09 18:10:27
-*/
-
 const path = require('path')
 const fs = require('fs-extra')
-const chalk = require('chalk')
-const pkgJson = require('../package.json')
-const program = require('commander')
 const shell = require('shelljs')
 
-const script = process.argv[2]
-const args = process.argv.slice(3)
 
-// cli
-program.version(pkgJson.version)
-       .description('🐱  create an alternative workflow project')
-       .option('-n, --name <name>', 'project name')
-program.parse(process.argv)
+const args = process.argv.slice(2) // [<module_name>, <project_name>]
+const moduleName = args[0]
+const projectName = args[1]
 
-
-// create target
-let selectVersion = script
-let projectName = args[1]
-const releaseSource = 'https://github.com/muzidx/totoro/archive/v1.0.0.tar.gz'
 const root = path.resolve(projectName)
+const totoroDir = path.resolve('totoro')
 
-if (selectVersion && projectName) {
-  shell.exec(`wget ${releaseSource} --no-check-certificate -P ${root}`)
-  shell.exec(`tar xzvf ${root}/v1.0.0.tar.gz -C ${root}`)
-  fs.copySync(`${root}/totoro-1.0.0/package/${selectVersion}`, root)
-  shell.exec(`rm -rf ${root}/v1.0.0.tar.gz`)
-  shell.exec(`rm -rf ${root}/totoro-1.0.0`)
+fs.copySync(`${totoroDir}/packages/${moduleName}`, root)
 
-  console.log(chalk.bold.green('Congratulations!!! create project success 🐱'))
-} else {
-  console.log(chalk.bold.red('please check the cli param, or use `totoro -h` to view the command!'))
-  process.exit(1)
-}
+const pkgJson = require(`${root}/package.json`)
+pkgJson.name = projectName
+
+fs.writeFileSync(
+  path.join(root, 'package.json'),
+  JSON.stringify(pkgJson, null, 2)
+)
+
+shell.exec('yarn install')
---
+++ cli/tar.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const fs = require('fs-extra')
+const chalk = require('chalk')
+const pkgJson = require('../package.json')
+const program = require('commander')
+const shell = require('shelljs')
+
+const script = process.argv[2]
+const args = process.argv.slice(3)
+
+// cli
+program.version(pkgJson.version)
+       .description('🐱  create an alternative workflow project')
+       .option('-n, --name <name>', 'project name')
+program.parse(process.argv)
+
+// create target
+let selectVersion = script
+let projectName = args[1]
+const releaseSource = 'https://github.com/muzidx/totoro/archive/v1.0.0.tar.gz'
+const root = path.resolve(projectName)
+
+if (selectVersion && projectName) {
+  shell.exec(`wget ${releaseSource} --no-check-certificate -P ${root}`)
+  shell.exec(`tar xzvf ${root}/v1.0.0.tar.gz -C ${root}`)
+  fs.copySync(`${root}/totoro-1.0.0/package/${selectVersion}`, root)
+  shell.exec(`rm -rf ${root}/v1.0.0.tar.gz`)
+  shell.exec(`rm -rf ${root}/totoro-1.0.0`)
+
+  console.log(chalk.bold.green('Congratulations!!! create project success 🐱'))
+} else {
+  console.log(chalk.bold.red('please check the cli param, or use `totoro -h` to view the command!'))
+  process.exit(1)
+}
